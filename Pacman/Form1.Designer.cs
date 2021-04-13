e3d5a939f0130bcf64bc0ecde309843c7bed364e
Tundraman300 <brnunley1@gmail.com>
2021-04-13 15:11:25 -0500

    Sizing

--- Pacman/Form1.Designer.cs
+++ Pacman/Form1.Designer.cs
@@ -29,54 +29,51 @@ namespace Pacman
         /// </summary>
         private void InitializeComponent()
         {
-            this.wall_1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.wall_1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.wall_1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // wall_1
-            // 
-            this.wall_1.BackColor = System.Drawing.Color.Blue;
-            this.wall_1.Location = new System.Drawing.Point(237, 71);
-            this.wall_1.Margin = new System.Windows.Forms.Padding(4);
-            this.wall_1.Name = "wall_1";
-            this.wall_1.Size = new System.Drawing.Size(133, 62);
-            this.wall_1.TabIndex = 0;
-            this.wall_1.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(381, 213);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(286, 173);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 17);
+            this.label1.Size = new System.Drawing.Size(70, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "This is a Test";
-            
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(644, 296);
+            this.label2.Location = new System.Drawing.Point(483, 240);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 17);
+            this.label2.Size = new System.Drawing.Size(62, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Test editing";
             // 
+            // wall_1
+            // 
+            this.wall_1.BackColor = System.Drawing.Color.Blue;
+            this.wall_1.Location = new System.Drawing.Point(178, 58);
+            this.wall_1.Name = "wall_1";
+            this.wall_1.Size = new System.Drawing.Size(100, 50);
+            this.wall_1.TabIndex = 0;
+            this.wall_1.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(634, 711);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.wall_1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Pacman";
             ((System.ComponentModel.ISupportInitialize)(this.wall_1)).EndInit();
@@ -86,10 +83,9 @@ namespace Pacman
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox wall_1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox wall_1;
     }
 }
 

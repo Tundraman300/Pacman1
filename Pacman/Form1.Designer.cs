3117290edac714bd758dd411555c56cd89e86842
Johncassetta <johncassetta@letu.edu>
2021-04-13 14:56:20 -0500

    Added test label

--- Pacman/Form1.Designer.cs
+++ Pacman/Form1.Designer.cs
@@ -31,15 +31,17 @@ namespace Pacman
         {
             this.wall_1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.wall_1)).BeginInit();
             this.SuspendLayout();
             // 
             // wall_1
             // 
             this.wall_1.BackColor = System.Drawing.Color.Blue;
-            this.wall_1.Location = new System.Drawing.Point(178, 58);
+            this.wall_1.Location = new System.Drawing.Point(237, 71);
+            this.wall_1.Margin = new System.Windows.Forms.Padding(4);
             this.wall_1.Name = "wall_1";
-            this.wall_1.Size = new System.Drawing.Size(100, 50);
+            this.wall_1.Size = new System.Drawing.Size(133, 62);
             this.wall_1.TabIndex = 0;
             this.wall_1.TabStop = false;
             // 
@@ -47,20 +49,34 @@ namespace Pacman
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(398, 269);
+            this.label1.Location = new System.Drawing.Point(381, 213);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.Size = new System.Drawing.Size(93, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "This is a Test";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(644, 296);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Test editing";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.wall_1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Pacman";
             ((System.ComponentModel.ISupportInitialize)(this.wall_1)).EndInit();
@@ -73,6 +89,7 @@ namespace Pacman
 
         private System.Windows.Forms.PictureBox wall_1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
